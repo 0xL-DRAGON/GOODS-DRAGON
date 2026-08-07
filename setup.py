3b071a5b92b6e8a3e016e365b89431da3dd482e3
zeus(z4) <support.zeus.z4@gmail.com>
2026-08-07 20:44:56 +0330

    Sync version to 2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="goods-dragon",
-    version="2.0.3",
+    version="2.0.0",
     packages=find_packages(),
     py_modules=["main"],
     install_requires=[
