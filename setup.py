e887087eb872ce0bce990ad914268d76ce74719b
zeus(z4) <support.zeus.z4@gmail.com>
2026-08-07 20:11:44 +0330

    Fix pip entry point with setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="goods-dragon",
-    version="2.0.2",
+    version="2.0.3",
     packages=find_packages(),
     py_modules=["main"],
     install_requires=[
